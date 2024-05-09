a1b1b5c9109005172cd4cce570ba7dda1e130593
hwanhee0728 <155794791+hwanhee0728@users.noreply.github.com>
2024-05-09 12:22:01 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -70,10 +70,6 @@ def app():
         st.write(':unicorn_face:선정 : 2만원 * 2명')
         st.write("")
         st.write("")
-        st.write("")
-        st.write("")
-        st.write("")
-        st.write("")
 
         # 엑셀 다운로드
         password = st.text_input(":computer: 관리자", type="password")
